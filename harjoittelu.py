191523e889e037eeb4503cf1d7d03444533e4d88
AlHelk <ahelkkula@gmail.com>
2021-12-16 21:15:28 +0200

    muokkaus

--- harjoittelu.py
+++
@@ -1,8 +0,0 @@
-def kopioi_oikeat_rivit():
-    import re
-    with open("lahde.txt") as tiedosto:
-        for rivi in tiedosto:
-            korjatut = re.search('^[A-Z][^?!.]*[.]$', rivi)
-
-    with open("kohde.txt", "w") as tiedosto2:
-                tiedosto2.write(korjatut)
--- luento1.py
+++
@@ -1,7 +0,0 @@
-lista = [-12, 3, -65]
-
-def muuta_itseisarvoiksi(lista: list):
-    itseisarvo = [abs(ele) for ele in lista]
-    return itseisarvo
-
-print(muuta_itseisarvoiksi(lista))
