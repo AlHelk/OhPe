7afaf27694991cef199a501541b00800c85725c7
AlHelk <ahelkkula@gmail.com>
2021-12-19 14:37:44 +0200

    Co-authored-by: eemilsuominen <eemilsuominen@users.noreply.github.com>

--- varaus.py
+++ varaus.py
@@ -5,7 +5,7 @@ salit = {"Sali 1": 50, "Sali 2": 45, "Sali 3": 70}
 naytokset = []
 
 def aloitus():
-    print("Tervetuloa huoramaiseen varaussysteemiin.\nKiitos että nuolit perseen ja mulkun -Anssi.")
+    print("Tervetuloa varaussysteemiin.")
     while True:
         rooli = input("Valitse rooli (Asiakas / Ylläpitäjä): ")
         if rooli == "Asiakas":
@@ -14,7 +14,7 @@ def aloitus():
         elif rooli == "Ylläpitäjä":
             while True:
                 salasana = input("Anna salasana: ")
-                if salasana == "kiveskiertymä":
+                if salasana == "salasana":
                     yllapitaja_rooli()
                     break
                 else:
@@ -24,36 +24,59 @@ def aloitus():
             print("Ei hyväksyttävä rooli, valitse Asiakas tai Ylläpitäjä.")
 
 def asiakas_rooli():
-    valinta_asiakas = input("Haluatko varata paikan vai tarkastella näytöksiä? Valitse (Varaus / Tarkastelu): ")
-    if valinta_asiakas == "Varaus":
-        elokuva = input("Valitse elokuva: ")
-        for naytos in naytokset:
-            if naytos[0] == elokuva:
-                aika = naytos[4].strftime("%d.%m %H:%M")
-                print(f"Ajankohta: {aika}, sali: {naytos[2]}, Vapaita paikkoja: {naytos[3]}, Id: {naytos[5]}")
-        varaus = int(input("Valitse id: "))
-        maara = int(input("Kuinka monta paikkaa haluat varata: "))
-        for i in naytokset:
-            if i[5] == varaus:
-                varaa_paikka(i, maara)
+    while True:
+        valinta_asiakas = input("Haluatko varata paikan vai tarkastella näytöksiä? Valitse (Varaus / Tarkastelu / Lopeta): ")
+        if valinta_asiakas == "Varaus":
+            varaus()
+        elif valinta_asiakas == "Tarkastelu":
+            selaa_naytoksia()
+        elif valinta_asiakas == "Lopeta":
+            break
+        else:
+            print("Ei kelvollinen valinta. Yritä uudelleen.")
 
 def yllapitaja_rooli():
-    print("Oikea salasana\nOnneksi olkoon, sait kiveskiertymän")
-    lisaa_naytos()
+    print("Oikea salasana")
+    while True:
+        valinta_yp = input("Haluatko lisätä vai tarkastella näytöksiä? (Lisää / Tarkastelu / Lopeta): ")
+        if valinta_yp == "Lisää":
+            lisaa_naytos()
+        elif valinta_yp == "Tarkastelu":
+            selaa_naytoksia()
+        elif valinta_yp == "Lopeta":
+            break
+        else:
+            print("Ei kelvollinen valinta. Yritä uudelleen.")
 
 def lataa_tiedosto():
-    with open("jotai.txt", "rb") as tiedosto:
+    with open("db.txt", "rb") as tiedosto:
         return pickle.load(tiedosto)
 
 def tallenna_tiedostoon():
-    with open("jotai.txt", "wb") as tiedosto:
+    with open("db.txt", "wb") as tiedosto:
         pickle.dump(naytokset, tiedosto) 
 
 def tallenna_naytos_listaan(elokuva: str, kesto: int, sali: str, pvm: str, klo: str, id):
-    ajankohta = datetime(int(pvm[2]), int(pvm[1]), int(pvm[0]), int(klo[0]), int(klo[1])) 
-    vapaat_paikat = salit[sali]
-    naytoksen_tiedot = (elokuva, kesto, sali, vapaat_paikat, ajankohta, id)
-    naytokset.append(naytoksen_tiedot)
+    try:
+        ajankohta = datetime(int(pvm[2]), int(pvm[1]), int(pvm[0]), int(klo[0]), int(klo[1])) 
+        vapaat_paikat = salit[sali]
+        naytoksen_tiedot = (elokuva, kesto, sali, vapaat_paikat, ajankohta, id)
+        naytokset.append(naytoksen_tiedot)
+    except:
+        print("Viimeisimmän näytöksen lisääminen epäonnistui. Yritä uudelleen")
+
+def varaus():
+    elokuva = input("Valitse elokuva: ")
+    for naytos in naytokset:
+        if naytos[0] == elokuva:
+            aika = naytos[4].strftime("%d.%m %H:%M")
+            print(f"Ajankohta: {aika}, sali: {naytos[2]}, Vapaita paikkoja: {naytos[3]}, Id: {naytos[5]}")
+    varaus = int(input("Valitse id: "))
+    maara = int(input("Kuinka monta paikkaa haluat varata: "))
+    for i in naytokset:
+        if i[5] == varaus:
+            varaa_paikka(i, maara)
+    
 
 def varaa_paikka(naytos, maara):
     a = list(naytos)
@@ -66,8 +89,6 @@ def varaa_paikka(naytos, maara):
     else:
         print("Salissa ei ole tarpeeksi tilaa!")
     
-def tulosta_naytos(naytos):
-    print(f"Elokuva: {naytos[0]}\nKesto: {naytos[1]} minuuttia\nAjankohta: {naytos[4]}\n{naytos[2]}\nVapaita paikkoja: {naytos[3]}\nId: {naytos[5]}")
 
 def lisaa_naytos():
     print("Näytösten ja elokuvien lisäys")
@@ -90,21 +111,44 @@ def lisaa_naytos():
                 n += 1
             tallenna_naytos_listaan(elokuva, kesto, sali, pvm, klo, naytos_id)
             
-        lopetus = input("Näytös lisätty, kirjoita 'lopeta' poistuaksesi lisäysjärjestelmästä, muutoin jätä tyhjäksi. ")
+        lopetus = input("Kirjoita 'lopeta' poistuaksesi lisäysjärjestelmästä, muutoin jätä tyhjäksi. ")
         if lopetus == "lopeta":
             break
 
 def selaa_naytoksia():
-    valinta = input("Valitse millä haluat hakea (Elokuva/Päivämäärä/Id/Näytä kaikki elokuvat)")
-    if valinta == "Elokuva":
-        ek = input("Valitse elokuva: ")
-        for i in naytokset:
-            if i[0] == ek:
-                pass
-    if valinta == "Päivämäärä":
-        pass
-    if valinta == "Id":
-        pass
+    while True:
+        valinta = input("Valitse millä haluat hakea (Elokuva / Päivämäärä / Id / Näytä kaikki elokuvat), (Taakse), jos haluat siirtyä taaksepäin,\ntai (Varaus) jos haluat siirtyä paikkavaraukseen: ")
+        if valinta == "Elokuva":
+            ek = input("Valitse elokuva: ")
+            for i in naytokset:
+                if i[0] == ek:
+                    print(f"Elokuva: {i[0]}, Ajankohta: {i[4]}, {i[2]}, Vapaita paikkoja: {i[3]}, Id: {i[5]}")
+        elif valinta == "Päivämäärä":
+            pvm_valinta = str(input("Valitse päivämäärä (päivä.kuukausi.vuosi): "))
+            for i in naytokset:
+                aika = i[4].strftime("%d.%m.%Y")
+                if aika == pvm_valinta:
+                    print(f"Elokuva: {i[0]}, Kesto: {i[1]} minuuttia, Ajankohta: {i[4]}, {i[2]}, Vapaita paikkoja: {i[3]}, Id: {i[5]}")
+        elif valinta == "Id":
+            id_valinta = int(input("Valitse id: "))
+            for n in naytokset:
+                if n[5] == id_valinta:
+                    print(f"Elokuva: {n[0]}, Kesto: {n[1]} minuuttia, Ajankohta: {n[4]}, {n[2]}, Vapaita paikkoja: {n[3]}, Id: {n[5]}")
+        elif valinta == "Näytä kaikki elokuvat":
+            elokuvat = {}
+            for i in naytokset:
+                if i[0] not in elokuvat:
+                    elokuvat[i[0]] = i[1]
+            for i in elokuvat:
+                print(f"{i}, kesto: {elokuvat[i]} minuuttia")
+        elif valinta == "Varaus":
+            varaus()
+            break
+        elif valinta == "Taakse":
+            break
+
+        else:
+            print("Ei kelvollinen valinta. Yritä uudelleen.")
 
 def monta_naytosta(kertoja: int):
     elokuva = str(input("Anna elokuvan nimi: "))
@@ -163,7 +207,7 @@ def monta_naytosta(kertoja: int):
                     else:
                         defg[0] += 1              
             elif defg[1] == 2:
-                if defg[2] == defg[2] % 4 == 0 and (defg[2] % 100 != 0 or defg[2] % 400 == 0):
+                if defg[2] % 4 == 0 and (defg[2] % 100 != 0 or defg[2] % 400 == 0):
                     if defg[0] == 29:
                         defg[0] = 1
                         defg[1] += 1
